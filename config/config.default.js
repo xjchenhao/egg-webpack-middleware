28052a0d86e012cb799fcc69e4d585b1f95160df
xjchenhao <wy.chenhao@qq.com>
2017-05-22 15:33:14 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * egg-webpack-middleware default config
+ * @member Config#webpackMiddleware
+ * @property {String} SOME_KEY - some description
+ */
+exports.webpackMiddleware = {
+
+};
