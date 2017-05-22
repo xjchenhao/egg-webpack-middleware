38aeaad31634440b7a3e31aa30b422cfac85d694
xjchenhao <wy.chenhao@qq.com>
2017-05-22 15:55:55 +0800

    v1.0.0

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-'use strict';
+let webpack = require('webpack');
+let webpackDevMiddleware = require('koa-webpack-dev-middleware');
+let webpackHotMiddleware = require('koa-webpack-hot-middleware');
 
 module.exports = app => {
-  console.log('app.config.env =', app.config.env);
+    app.addSingleton('webpackMiddleware', webpackHot);
 };
+
+function webpackHot(config, app) {
+    let compiler = webpack(config);
+
+    app.use(webpackDevMiddleware(compiler, {
+        noInfo: true,
+        publicPath: config.output.publicPath
+    }));
+
+    app.use(webpackHotMiddleware(compiler));
+}
